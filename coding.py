62251bc6969016503e86fc2da14e05af716c6e30
yunj <yunj8649@xingxingmobility.com>
2021-11-28 18:45:09 +0900

    Initial commit

---
+++ coding.py
@@ -0,0 +1,12 @@
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(len(array)):
+    min_index = i
+    print(f'-------------{i}--------------------')
+    for j in range(i + 1, len(array)):
+        print('min_index :', min_index)
+        if array[min_index] > array[j]:
+            min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+
+print(array)
